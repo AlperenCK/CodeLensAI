1c71bdcb4ec55ea1849a98456fe61a2f4dd51112
AlperenCK <132698544+AlperenCK@users.noreply.github.com>
2026-06-17 07:40:53 +0300

    feat: add CodeLensAI/Properties/AssemblyInfo.cs

---
+++ CodeLensAI/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeLensAI")]
+[assembly: AssemblyDescription("Local LLM Code Analysis Extension for Visual Studio 2022")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CodeLensAI Team")]
+[assembly: AssemblyProduct("CodeLens AI")]
+[assembly: AssemblyCopyright("Copyright © 2026 CodeLensAI Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
